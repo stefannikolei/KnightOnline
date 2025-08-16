eb76194268883a81bc0019326b362258897ef756
twostars <i.am.twostars@gmail.com>
2025-08-16 12:51:51 +1000

    Restore back button for character selection
    Resolves #44

--- Client/WarFare/UICharacterSelect.h
+++ Client/WarFare/UICharacterSelect.h
@@ -11,27 +11,30 @@
 
 #include <N3Base/N3UIBase.h>
 
+struct __CharacterSelectInfo;
 class CUICharacterSelect : public CN3UIBase
 {
 protected:
-	CN3UIBase* m_pBtnLeft;
-	CN3UIBase* m_pBtnRight;
-	CN3UIBase* m_pBtnExit;
-	CN3UIBase* m_pBtnDelete;
+	CN3UIBase*		m_pBtnLeft;
+	CN3UIBase*		m_pBtnRight;
+	CN3UIBase*		m_pBtnExit;
+	CN3UIBase*		m_pBtnDelete;
+	CN3UIBase*		m_pBtnBack;
+	CN3UIString*	m_pUserInfoStr;
 
 public:
-	uint32_t MouseProc(uint32_t dwFlags, const POINT& ptCur, const POINT& ptOld);
-	bool OnKeyPress(int iKey);
+	uint32_t MouseProc(uint32_t dwFlags, const POINT& ptCur, const POINT& ptOld) override;
+	bool OnKeyPress(int iKey) override;
 	CUICharacterSelect();
-	virtual ~CUICharacterSelect();
+	~CUICharacterSelect() override;
 
-	virtual void	Release();
-	virtual bool	Load(HANDLE hFile);
-	virtual void	Tick();
-	virtual bool	ReceiveMessage(CN3UIBase* pSender, uint32_t dwMsg);
+	void Release() override;
+	bool Load(HANDLE hFile) override;
+	void Tick() override;
+	bool ReceiveMessage(CN3UIBase* pSender, uint32_t dwMsg) override;
 
-	void	DisplayChrInfo(__CharacterSelectInfo* pCSInfo);
-	void	DontDisplayInfo();
+	void DisplayChrInfo(__CharacterSelectInfo* pCSInfo);
+	void DontDisplayInfo();
 };
 
 #endif // !defined(AFX_UICharacterSelect_H__665CADA6_E25B_47D6_B962_6DA49673048F__INCLUDED_)
