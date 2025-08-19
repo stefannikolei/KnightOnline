f02b668b8461c123ec98d7b21efa11f0290cfa86
koraykarakus <65832770+koraykarakus@users.noreply.github.com>
2025-08-19 17:58:41 +0300

    Merge pull request #449 from koraykarakus/uidead-cleanup
    CUIDead: Reformatter pass + minor code cleanup

--- Client/WarFare/UIDead.cpp
+++ Client/WarFare/UIDead.cpp
@@ -39,33 +39,32 @@ static char THIS_FILE[]=__FILE__;
 
 CUIDead::CUIDead()
 {
-	m_pTextAlive	= NULL;
-	m_pTextTown		= NULL;
+	m_pTextAlive	= nullptr;
+	m_pTextTown		= nullptr;
 	m_bProcessing	= false;
 }
 
 CUIDead::~CUIDead()
 {
-
 }
 
 bool CUIDead::Load(HANDLE hFile)
 {
-	if(false == CN3UIBase::Load(hFile)) return false;
+	if (!CN3UIBase::Load(hFile))
+		return false;
 
-	m_pTextAlive	= (CN3UIString*)(this->GetChildByID("Text_Alive"));	__ASSERT(m_pTextAlive, "NULL UI Component!!!");
-	m_pTextTown		= (CN3UIString*)(this->GetChildByID("Text_Town"));	__ASSERT(m_pTextTown, "NULL UI Component!!!");
-
+	N3_VERIFY_UI_COMPONENT(m_pTextAlive,	GetChildByID<CN3UIString>("Text_Alive"));
+	N3_VERIFY_UI_COMPONENT(m_pTextTown,		GetChildByID<CN3UIString>("Text_Town"));
 
 	std::string szMsg = fmt::format_text_resource(IDS_DEAD_REVIVAL);
-	if(m_pTextAlive) m_pTextAlive->SetString(szMsg);
+	if (m_pTextAlive != nullptr)
+		m_pTextAlive->SetString(szMsg);
 
 	szMsg = fmt::format_text_resource(IDS_DEAD_RETURN_TOWN);
-	if(m_pTextTown) m_pTextTown->SetString(szMsg);
+	if (m_pTextTown != nullptr)
+		m_pTextTown->SetString(szMsg);
 
-	__TABLE_UI_RESRC*	pTblUI	= NULL;
-	pTblUI = CGameBase::s_pTbl_UI.Find(NATION_ELMORAD);
-
+	__TABLE_UI_RESRC* pTblUI = CGameBase::s_pTbl_UI.Find(NATION_ELMORAD);
 	m_MsgBox.LoadFromFile(pTblUI->szMessageBox);
 
 	RECT rt = m_MsgBox.GetRegion();
@@ -77,14 +76,14 @@ bool CUIDead::Load(HANDLE hFile)
 	return true;
 }
 
-bool CUIDead::ReceiveMessage(CN3UIBase *pSender, uint32_t dwMsg)
+bool CUIDead::ReceiveMessage(CN3UIBase* pSender, uint32_t dwMsg)
 {
-	if( dwMsg == UIMSG_STRING_LCLICK )
+	if (dwMsg == UIMSG_STRING_LCLICK)
 	{
-		if(pSender == m_pTextAlive)
+		if (pSender == m_pTextAlive)
 		{
 			int iItemCnt = 0, iLevel = 0, iNeedItemCnt = 0;
-			if(CGameProcedure::s_pProcMain->m_pUIInventory)
+			if (CGameProcedure::s_pProcMain->m_pUIInventory != nullptr)
 				iItemCnt = CGameProcedure::s_pProcMain->m_pUIInventory->GetIndexItemCount(LIFE_STONE_INDEX);
 
 			iLevel = CGameBase::s_pPlayer->m_InfoBase.iLevel;
@@ -131,32 +130,31 @@ bool CUIDead::ReceiveMessage(CN3UIBase *pSender, uint32_t dwMsg)
 	return true;
 }
 
-uint32_t CUIDead::MouseProc(uint32_t dwFlags, const POINT &ptCur, const POINT &ptOld)
+uint32_t CUIDead::MouseProc(uint32_t dwFlags, const POINT& ptCur, const POINT& ptOld)
 {
 	uint32_t dwRet = UI_MOUSEPROC_NONE;
-	if (!m_bVisible) return dwRet;
+	if (!m_bVisible)
+		return dwRet;
 
 	// UI 움직이는 코드
 	if (UI_STATE_COMMON_MOVE == m_eState)
 	{
-		if (dwFlags&UI_MOUSE_LBCLICKED)
-		{
+		if (dwFlags & UI_MOUSE_LBCLICKED)
 			SetState(UI_STATE_COMMON_NONE);
-		}
 		else
-		{
 			MoveOffset(ptCur.x - ptOld.x, ptCur.y - ptOld.y);
-		}
+
 		dwRet |= UI_MOUSEPROC_DONESOMETHING;
 		return dwRet;
 	}
 
-	if(false == IsIn(ptCur.x, ptCur.y))	// 영역 밖이면
+	// 영역 밖이면
+	if (!IsIn(ptCur.x, ptCur.y))
 	{
-		if(false == IsIn(ptOld.x, ptOld.y))
-		{
-			return dwRet;// 이전 좌표도 영역 밖이면 
-		}
+		// 이전 좌표도 영역 밖이면 
+		if (!IsIn(ptOld.x, ptOld.y))
+			return dwRet;
+
 		dwRet |= UI_MOUSEPROC_PREVINREGION;	// 이전 좌표는 영역 안이었다.
 	}
 	else
@@ -168,43 +166,41 @@ uint32_t CUIDead::MouseProc(uint32_t dwFlags, const POINT &ptCur, const POINT &p
 
 	dwRet |= UI_MOUSEPROC_INREGION;	// 이번 좌표는 영역 안이다.
 
-	if(m_pChildUI && m_pChildUI->IsVisible())
+	if (m_pChildUI != nullptr && m_pChildUI->IsVisible())
 		return dwRet;
 
 	// child에게 메세지 전달
-	for(UIListItor itor = m_Children.begin(); m_Children.end() != itor; ++itor)
+	for (UIListItor itor = m_Children.begin(); m_Children.end() != itor; ++itor)
 	{
 		CN3UIBase* pChild = (*itor);
 		uint32_t dwChildRet = 0;
 
 		dwChildRet = pChild->MouseProc(dwFlags, ptCur, ptOld);
-		if( pChild->IsVisible() && UI_TYPE_STRING == pChild->UIType() )
+		if (pChild->IsVisible() && UI_TYPE_STRING == pChild->UIType())
 		{
-			if(pChild->IsIn(ptCur.x, ptCur.y) && (dwFlags & UI_MOUSE_LBCLICKED) )	
-			{
+			if (pChild->IsIn(ptCur.x, ptCur.y) && (dwFlags & UI_MOUSE_LBCLICKED))
 				dwChildRet |= UI_MOUSEPROC_DONESOMETHING;
-			}
 
-			if(pChild->IsIn(ptCur.x, ptCur.y) && (dwFlags & UI_MOUSE_LBDBLCLK) )	
-			{
+			if (pChild->IsIn(ptCur.x, ptCur.y) && (dwFlags & UI_MOUSE_LBDBLCLK))
 				dwChildRet |= UI_MOUSEPROC_DONESOMETHING;
-			}
 		}
 
+		// 이경우에는 먼가 포커스를 받은 경우이다.
 		if (UI_MOUSEPROC_DONESOMETHING & dwChildRet)
-		{	// 이경우에는 먼가 포커스를 받은 경우이다.
+		{
 			// (아래 코드는 dialog를 관리하는 곳에서 해야 한다. 따라서 막아놓음)
 //			m_Children.erase(itor);			// 우선 리스트에서 지우고
 //			m_Children.push_front(pChild);	// 맨앞에 넣는다. 그리는 순서를 맨 나중에 그리도록 하려고
 
-			dwRet |= (UI_MOUSEPROC_CHILDDONESOMETHING|UI_MOUSEPROC_DONESOMETHING);
+			dwRet |= (UI_MOUSEPROC_CHILDDONESOMETHING | UI_MOUSEPROC_DONESOMETHING);
 			return dwRet;
 		}
 	}
 
 	// UI 움직이는 코드
-	if (UI_STATE_COMMON_MOVE != m_eState && 
-			PtInRect(&m_rcMovable, ptCur) && (dwFlags&UI_MOUSE_LBCLICK) )
+	if (UI_STATE_COMMON_MOVE != m_eState
+		&& PtInRect(&m_rcMovable, ptCur)
+		&& (dwFlags & UI_MOUSE_LBCLICK))
 	{
 		SetState(UI_STATE_COMMON_MOVE);
 		dwRet |= UI_MOUSEPROC_DONESOMETHING;
@@ -218,31 +214,31 @@ void CUIDead::CallBackProc(int iID, uint32_t dwFlag)
 {
 	//TRACE("OnButton ID:%d Btn %d\n",iID, dwFlag);
 
-	if(iID == CHILD_UI_REVIVE_MSG)
+	if (iID == CHILD_UI_REVIVE_MSG)
 	{
-		if(dwFlag == 1)//OK
-		{
+		// OK
+		if (dwFlag == 1)
 			MsgSend_Revival(REVIVAL_TYPE_LIFE_STONE);
-		}
 	}
-	else if(iID == CHILD_UI_LACK_LIVE_STONE_MSG)
+	else if (iID == CHILD_UI_LACK_LIVE_STONE_MSG)
 	{
 	}
-	else if(iID == CHILD_UI_LOW_LEVEL)
+	else if (iID == CHILD_UI_LOW_LEVEL)
 	{
 	}
 }
 
 void CUIDead::MsgSend_Revival(uint8_t byType)
 {
-	if(m_bProcessing) return;
+	if (m_bProcessing)
+		return;
 
 	// 한번 보내면 다시 죽을때까지 안보내는 플래그
 	if (CGameBase::s_pPlayer->m_iSendRegeneration >= 2)
 		return;
 
 	uint8_t byBuff[4];
-	int iOffset=0;
+	int iOffset = 0;
 
 	CAPISocket::MP_AddByte(byBuff, iOffset, WIZ_REGENE);
 	CAPISocket::MP_AddByte(byBuff, iOffset, byType);
@@ -260,32 +256,40 @@ void CUIDead::MsgRecv_Revival(Packet& pkt)
 	m_bProcessing = false;
 
 	__Vector3 vPosPlayer;
-	vPosPlayer.x = (pkt.read<uint16_t>())/10.0f;
-	vPosPlayer.z = (pkt.read<uint16_t>())/10.0f;
-	vPosPlayer.y = (pkt.read<int16_t>())/10.0f;
-	
-	CGameProcedure::s_pProcMain->InitPlayerPosition(vPosPlayer); // 플레이어 위치 초기화.. 일으켜 세우고, 기본동작을 취하게 한다.
-	CGameBase::s_pPlayer->RegenerateCollisionMesh(); // 충돌 메시를 다시 만든다..
+	vPosPlayer.x = static_cast<float>(pkt.read<uint16_t>()) / 10.0f;
+	vPosPlayer.z = static_cast<float>(pkt.read<uint16_t>()) / 10.0f;
+	vPosPlayer.y = static_cast<float>(pkt.read<int16_t>()) / 10.0f;
 
-	CGameBase::s_pPlayer->m_iSendRegeneration = 0; // 한번 보내면 다시 죽을때까지 안보내는 플래그
-	CGameBase::s_pPlayer->m_fTimeAfterDeath = 0; // 한번 보내면 다시 죽을때까지 안보내는 플래그
-	//TRACE("받음 - 다시 살아나기(%.1f, %.1f)\n", vPosPlayer.x, vPosPlayer.z);
+	// 플레이어 위치 초기화.. 일으켜 세우고, 기본동작을 취하게 한다.
+	CGameProcedure::s_pProcMain->InitPlayerPosition(vPosPlayer);
 
-	//
-	//마법 & 효과 초기화..
-	if(CGameProcedure::s_pProcMain->m_pUIStateBarAndMiniMap) 
+	// 충돌 메시를 다시 만든다..
+	CGameBase::s_pPlayer->RegenerateCollisionMesh();
+
+	// 한번 보내면 다시 죽을때까지 안보내는 플래그
+	CGameBase::s_pPlayer->m_iSendRegeneration = 0;
+
+	// 한번 보내면 다시 죽을때까지 안보내는 플래그
+	CGameBase::s_pPlayer->m_fTimeAfterDeath = 0;
+
+	// TRACE("받음 - 다시 살아나기(%.1f, %.1f)\n", vPosPlayer.x, vPosPlayer.z);
+
+	// 마법 & 효과 초기화..
+	if (CGameProcedure::s_pProcMain->m_pUIStateBarAndMiniMap != nullptr)
 		CGameProcedure::s_pProcMain->m_pUIStateBarAndMiniMap->ClearMagic();
-	if(CGameProcedure::s_pProcMain->m_pMagicSkillMng) 
+
+	if (CGameProcedure::s_pProcMain->m_pMagicSkillMng != nullptr)
 		CGameProcedure::s_pProcMain->m_pMagicSkillMng->ClearDurationalMagic();
-	if(CGameProcedure::s_pProcMain->CGameProcedure::s_pFX) 
+
+	if (CGameProcedure::s_pFX != nullptr)
 		CGameProcedure::s_pFX->StopMine();
 
 	CLogWriter::Write("Receive Regeneration");
 
 	int iID = CGameBase::s_pPlayer->IDNumber();
-	if(CGameBase::s_pPlayer->Nation()==NATION_KARUS) 
+	if (CGameBase::s_pPlayer->Nation() == NATION_KARUS)
 		CGameProcedure::s_pFX->TriggerBundle(iID, -1, FXID_REGEN_KARUS, iID, -1);
-	else if(CGameBase::s_pPlayer->Nation()==NATION_ELMORAD) 
+	else if (CGameBase::s_pPlayer->Nation() == NATION_ELMORAD)
 		CGameProcedure::s_pFX->TriggerBundle(iID, -1, FXID_REGEN_ELMORAD, iID, -1);
 
 	SetVisible(false);
@@ -294,7 +298,7 @@ void CUIDead::MsgRecv_Revival(Packet& pkt)
 void CUIDead::SetVisible(bool bVisible)
 {
 	CN3UIBase::SetVisible(bVisible);
-	if(bVisible)
+	if (bVisible)
 		CGameProcedure::s_pUIMgr->SetVisibleFocusedUI(this);
 	else
 		CGameProcedure::s_pUIMgr->ReFocusUI();//this_ui
@@ -302,8 +306,6 @@ void CUIDead::SetVisible(bool bVisible)
 
 void CUIDead::SetVisibleWithNoSound(bool bVisible, bool bWork, bool bReFocus)
 {
-	if(!bWork)
-	{
+	if (!bWork)
 		CN3UIBase::SetVisibleWithNoSound(bVisible, bWork, bReFocus);
-	}
 }
--- Client/WarFare/UIDead.h
+++ Client/WarFare/UIDead.h
@@ -12,7 +12,7 @@
 #include <N3Base/N3UIBase.h>
 #include "UIMessageBox.h"
 
-class CUIDead : public CN3UIBase  
+class CUIDead : public CN3UIBase
 {
 protected:
 	class CN3UIString*		m_pTextAlive;
@@ -21,17 +21,16 @@ protected:
 	bool					m_bProcessing;
 
 public:
-	void SetVisibleWithNoSound(bool bVisible, bool bWork = false, bool bReFocus = false);
-	void SetVisible(bool bVisible);
+	CUIDead();
+	~CUIDead() override;
+	void SetVisibleWithNoSound(bool bVisible, bool bWork = false, bool bReFocus = false) override;
+	void SetVisible(bool bVisible) override;
 	void MsgRecv_Revival(Packet& pkt);
 	void MsgSend_Revival(uint8_t byType);
-	void CallBackProc(int iID, uint32_t dwFlag);
-	uint32_t MouseProc(uint32_t dwFlags, const POINT &ptCur, const POINT &ptOld);
-	bool ReceiveMessage(CN3UIBase *pSender, uint32_t dwMsg);
-	bool Load(HANDLE hFile);
-	CUIDead();
-	virtual ~CUIDead();
-
+	void CallBackProc(int iID, uint32_t dwFlag) override;
+	uint32_t MouseProc(uint32_t dwFlags, const POINT &ptCur, const POINT &ptOld) override;
+	bool ReceiveMessage(CN3UIBase *pSender, uint32_t dwMsg) override;
+	bool Load(HANDLE hFile) override;
 };
 
 #endif // !defined(AFX_UIDEAD_H__B5FAFAB6_04A4_4708_945E_731AE35257EB__INCLUDED_)
